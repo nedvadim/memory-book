1b5aa5e6b2f78b43e6a1565d32c7cdd60f31d243
Vadim Nedelenko <nedvadim@gmail.com>
2020-10-31 23:19:58 +0200

    sign up component restyle

--- src/components/AuthComponents/SignUpPage/SignUp.js
+++ src/components/AuthComponents/SignUpPage/SignUp.js
@@ -1,30 +1,71 @@
-import React, { useCallback } from 'react';
+import React, {useCallback, useState} from 'react';
 import { withRouter } from "react-router";
 import { Link } from 'react-router-dom';
 import app from "../../../firebase/base"
+import classes from "./SignUp.module.css"
+import { TextField, Card, CardContent, Typography, CardActions, Button, Grid } from '@material-ui/core'
+
 const SignUp = ({history}) => {
-    const handleSignUp = useCallback(async event => {
-        event.preventDefault();
-        const { email, password } = event.target.elements;
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const handleSignUp = async () => {
+        console.log(email, password);
         try {
-            await app.auth().createUserWithEmailAndPassword(email.value, password.value);
+            await app.auth().createUserWithEmailAndPassword(email, password);
             history.push('/');
         } catch (e) {
             alert(e);
         }
-    }, [history]);
+    };
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
     return (
         <>
-            <h2>Sign up</h2>
-            <form onSubmit={handleSignUp}>
-                <input name='email' type="email" placeholder='email'/>
-                <input name='password' type="password" placeholder='password'/>
-                <button type='submit'>Sign Up</button>
-            </form>
-            <p>Already have an account?</p>
-            <Link to="/login">
-                <p>Log in</p>
-            </Link>
+            <Card className={classes.SignUpCard}>
+                <CardContent>
+                    <Typography variant="h3" align="center" pb="15rem"> Sign up</Typography>
+                    <Grid container>
+                        <Grid item xs={12}>
+                            <TextField
+                                label="Email"
+                                type="email"
+                                className={classes.SignUpCardInput}
+                                value={email}
+                                onChange={handleEmailChange}/>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                label="Password"
+                                type="password"
+                                className={classes.SignUpCardInput}
+                                value={password}
+                                onChange={handlePasswordChange} />
+                        </Grid>
+                    </Grid>
+                </CardContent>
+                <CardActions>
+                    <Button variant="contained" color="primary" onClick={handleSignUp}>Sign up</Button>
+                    <Typography>
+                        Already has an account?</Typography>
+                        <Link to="/login">
+                            <Typography>Log in</Typography>
+                        </Link>
+                </CardActions>
+            </Card>
+            {/*<h2>Sign up</h2>*/}
+            {/*<form onSubmit={handleSignUp}>*/}
+            {/*    <input name='email' type="email" placeholder='email'/>*/}
+            {/*    <input name='password' type="password" placeholder='password'/>*/}
+            {/*    <button type='submit'>Sign Up</button>*/}
+            {/*</form>*/}
+            {/*<p>Already have an account?</p>*/}
+            {/*<Link to="/login">*/}
+            {/*    <p>Log in</p>*/}
+            {/*</Link>*/}
         </>
     )
  };
